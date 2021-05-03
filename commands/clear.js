4835741e30449a237a033bf4ddda9da1f1ef129c
OrkunAvci <orkunavci28@gmail.com>
2021-05-03 19:54:12 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports = {
 		const numToDelete = parseInt(args[0]);
 
 		if (isNaN(numToDelete)) {
-			return message.reply(`only arguments can be numbers. Try --clear 4.`);
+			return message.reply(`arguments can only be numbers. Try --clear 5.`);
 		} else if (numToDelete < 2 || 100 < numToDelete) {
 			return message.reply(`number must be between 2 and 100, and bot cannot delete messages older than two weeks.`);
 		}
